a706e618951129cf253e593ee28fd4af95108a14
vkohutid <vkohut@intelliarts.com>
2024-01-22 20:01:52 +0200

    feat: start working on ast and parser

---
+++ ast/ast.go
@@ -0,0 +1,46 @@
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	exporessionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Value Expression
+	Name  *Identifier
+	Token token.Token
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) exporessionNode()     {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
